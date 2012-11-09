304ab5bbec1de4c904a2358946aa7b05c30640b1
Ptotem Learning <info@ptotem.com>
2012-11-10 05:06:18 +0530

    Added Focus field for building Tag Trees

--- lib/acts_as_taggable_on/tag.rb
+++ lib/acts_as_taggable_on/tag.rb
@@ -11,7 +11,7 @@ module ActsAsTaggableOn
     ### VALIDATIONS:
 
     validates_presence_of :name
-    validates_uniqueness_of :name
+    validates_uniqueness_of :name, :scope=>:focus
     validates_length_of :name, :maximum => 255
 
     ### SCOPES:
